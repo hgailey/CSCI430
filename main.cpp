414e016d11ac159cd30846189c1fff644506a8d3
Halin Gailey <hgailey@csuchico.edu>
2025-02-03 11:28:40 -0800

    [fixes potential division by zero issue]

--- main.cpp
+++ main.cpp
@@ -13,7 +13,12 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    if(y == 0){
+	    cout << "division by zero" << end;
+    }
+    else{
+    	std::cout << "Division: " << x / y << std::endl;
+    }
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
